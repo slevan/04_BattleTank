c7cd38d5797820d25cb8bc94ee56299fb1858ef4
Michael Johnson <michaelfjohnson@gmail.com>
2017-01-03 16:05:49 -0800

    BT58 A Better Component Architecture

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -16,7 +16,6 @@ ATank::ATank()
 
 	// No need to protect points as needed at construction
 	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
-	TankMovementComponent = CreateDefaultSubobject<UTankMovementComponent>(FName("Movement Component"));
 }
 
 void ATank::SetBarrelReference(UTankBarrel* BarrelToSet)
