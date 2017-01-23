7520a491df00f0acf2975b71c186c64dbdeddc57
Michael Johnson <michaelfjohnson@gmail.com>
2017-01-22 20:19:38 -0800

    BT91 Using FAttachmentTransformRules

--- BattleTank/Source/BattleTank/Public/Projectile.h
+++ BattleTank/Source/BattleTank/Public/Projectile.h
@@ -16,13 +16,13 @@ public:
 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	
-	// Called every frame
-	virtual void Tick( float DeltaSeconds ) override;
 
 	void LaunchProjectile(float Speed);
 
 private:
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+
 	UProjectileMovementComponent* ProjectileMovement = nullptr;
 	
 	UPROPERTY(VisibleAnywhere, Category = "Componenents")
@@ -31,4 +31,6 @@ private:
 	UPROPERTY(VisibleAnywhere, Category = "Componenents")
 	UParticleSystemComponent* LaunchBlast = nullptr;
 
+	UPROPERTY(VisibleAnywhere, Category = "Componenents")
+	UParticleSystemComponent* ImpactBlast = nullptr;
 };
