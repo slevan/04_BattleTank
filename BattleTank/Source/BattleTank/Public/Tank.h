8376f7835c3491809e544b7c568fc7b19ab5b485
Michael Johnson <michaelfjohnson@gmail.com>
2017-01-24 22:06:24 -0800

    BT94 Using TakeDamage() on Actors

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -13,4 +13,16 @@ class BATTLETANK_API ATank : public APawn
 
 	// Sets default values for this pawn's properties
 	ATank();
+
+public:
+	// called by the engine when actor damage is dealt
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	int32 StartingHealth = 100;
+
+	UPROPERTY(VisibleAnywhere, Category = "Health")
+	int32 CurrentHealth = StartingHealth;
+
 };
